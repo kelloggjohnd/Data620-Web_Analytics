26d1c27c675543efb6420d2e7033cd6086a3f6e0
Jeff Shamp <shamp.jeff@gmail.com>
2021-03-15 20:56:04 -0400

    add alternate files

---
+++ utils_620.py
@@ -0,0 +1,27 @@
+# Import these to you notebook of choice
+
+def get_metric(metric_name, network):
+    metric_dict = {
+        "degree":nx.degree_centrality(network),
+        "eigen":nx.eigenvector_centrality_numpy(network),
+        "close":nx.closeness_centrality(network),
+        "between":nx.betweenness_centrality(network)
+    }
+    return metric_dict[metric_name]
+
+def make_centrality(network, metric_name):
+    metric = get_metric(metric_name, network)
+    _sort=sorted(metric.items(),
+          key=lambda x:x[1], 
+          reverse=True)
+    metric_df = pd.DataFrame(_sort, columns=['node',
+                                             f"{metric_name}_central"])
+    return metric_df
+def make_merge(df_list):
+    from functools import reduce
+    df_final = reduce(
+        lambda left,right: pd.merge(left,right,on='node',
+                                    how="inner"),
+        df_list
+    )
+    return df_final
